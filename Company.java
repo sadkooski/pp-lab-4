25402c89aa8fcfcf580e0482b0d15d488867cda3
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-04-07 16:50:04 +0200

    zad3

--- Company.java
+++ Company.java
@@ -5,13 +5,23 @@ public class Company {
     
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
-        
-        employees[0] = new Manager("Person One", 60000.0, 0);
-        employees[1] = new Worker("Person Two", 50000.0, "worker");
-        employees[2] = new Employee("Person Three", 55000.0);
-        employees[3] = new Employee("Person Four", 58000.0);
-        employees[4] = new Worker("Person Five", 52000.0, "worker");
+          
+        employees[0] = new Manager("Person One", 6000, 0);
+        employees[1] = new Worker("Person Two", 5000, "worker");
+        employees[2] = new Employee("Person Three", 5500);
+        employees[3] = new Employee("Person Four", 5800);
+        employees[4] = new Worker("Person Five", 5200, "worker");
 
+        int nonManagerEmployeesCount = 0;
+          for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+            nonManagerEmployeesCount++;
+            }
+        }
+        
+        ((Manager)employees[0]).setNumberOfSubordinates(nonManagerEmployeesCount);
+
+        employees[0].setSalary(7500);
         System.out.println("Dane wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
