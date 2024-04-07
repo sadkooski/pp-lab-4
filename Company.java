86b206a8523ad79f3718a222ed8da9a274534469
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-04-07 18:05:22 +0200

    poprawki

--- Company.java
+++ Company.java
@@ -8,7 +8,7 @@ public class Company {
         
         employees[0] = new Manager("Person One", 7000, 0);
         employees[1] = new Worker("Person Two", 5000, "worker");
-        employees[2] = new Employee("Person Three", 5500);
+        employees[2] = new Employee("Person Three", 5100);
         employees[3] = new Employee("Person Four", 5800);
         employees[4] = new Worker("Person Five", 5200, "worker");
 
