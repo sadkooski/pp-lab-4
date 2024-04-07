cfed8f756f27d312896424776b0ac52217fe592e
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-04-07 18:04:43 +0200

    poprawki

--- Company.java
+++ Company.java
@@ -4,15 +4,15 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Employee("Person One", 50000.0);
-        employees[1] = new Employee("Person Two", 60000.0);
-        employees[2] = new Employee("Person Three", 55000.0);
-        employees[3] = new Employee("Person Four", 52000.0);
-        employees[4] = new Employee("Person Five", 58000.0);
+        employees[0] = new Employee("Person One", 7000);
+        employees[1] = new Employee("Person Two", 6000);
+        employees[2] = new Employee("Person Three", 5500);
+        employees[3] = new Employee("Person Four", 5200);
+        employees[4] = new Employee("Person Five", 5800);
 
         System.out.println("Dane pracownika o indeksie 3: " + employees[3]);
 
-        employees[3].setSalary(100000.0);
+        employees[3].setSalary(10000);
 
         System.out.println("Dane wszystkich pracowników:");
         for (Employee employee : employees) {
